54b025c058907bb4eb8653beb118aecc74dfac02
Miguel <diaz4674@gmail.com>
2019-02-27 19:54:57 -0800

    Competed 2 stretch goals

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -3,13 +3,27 @@ import "./Footer.css";
 
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous"></link>
 
+
+const commentClicked = () => {
+    alert('You clicked me!')
+}
+
+const reTweet = () => {
+    alert('You Re-tweeted this!')
+}
+
+const heart = () => {
+    alert('You liked this!')
+}
+
+
 const Footer = () => {
-    return (
-        <div> 
-            <i class="far fa-comment"></i>
-            <i class="fas fa-sync-alt" />
-            <i class="far fa-heart" />
-            <i class="far fa-envelope" />
+    return (        <div className='icons'> 
+
+            <i class="far fa-comment" onClick = {commentClicked} ></i>
+            <i class="fas fa-sync-alt reTweet" onClick = {reTweet} > 6 </i>
+            <i class="far fa-heart heart" onClick = {heart}> 4 </i>
+            <a class="mailto" href="mailto:contact@test.com"> <i class="far fa-envelope email" /></a>
         </div>
     );
 }
