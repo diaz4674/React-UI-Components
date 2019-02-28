51cf1f5d34315bf6d6217e127c7302d534ee3009
Miguel <diaz4674@gmail.com>
2019-02-27 18:46:06 -0800

    Finished MVP

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = () => {
+    return (
+        <div className = 'ReactContainer'>
+            <h5>Get started with React</h5>
+            <p className = 'ReactContent'>React makes it painless to create interactive UIs. Design simple views for each state in your application.</p>
+           <a href ="https://reactjs.org/">reactjs.org</a>
+        </div>
+    )
+}
+
+export default CardContent;
---
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./Footer.css";
+
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous"></link>
+
+const Footer = () => {
+    return (
+        <div> 
+            <i class="far fa-comment"></i>
+            <i class="fas fa-sync-alt" />
+            <i class="far fa-heart" />
+            <i class="far fa-envelope" />
+        </div>
+    );
+}
+export default Footer;
